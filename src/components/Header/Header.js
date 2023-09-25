968e66676c8981a42dadf2781a1721078b72bc51
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-25 14:09:26 +0600

    feat: сверстать header в неавторизованном состоянии

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <Link className="header__logo" to="/" />
+      <div className="header__auth">
+        <Link className="header__link header__link_type_signup" to="/sign-up">
+          Регистрация
+        </Link>
+        <Link className="header__link header__link_type_signin" to="/sign-in">
+          Войти
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
