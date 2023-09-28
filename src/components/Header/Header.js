6424e2b6c69701f071acfad38e1995c94102226b
BuyanauskasAA <Buyanauskas@gmail.com>
2023-09-28 11:27:56 +0600

    feat: сверстать header в авторизованном состоянии

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
+  const loggedIn = React.useContext(CurrentUserContext);
   return (
     <header className="header">
       <Link className="header__logo" to="/" />
-      <div className="header__auth">
-        <Link className="header__link" to="/sign-up">
-          Регистрация
-        </Link>
-        <Link className="header__link header__link_type_signin" to="/sign-in">
-          Войти
-        </Link>
-      </div>
+      {loggedIn ? (
+        <>
+          <Navigation />
+          <Link className="header__link header__link_type_account" to="/profile">
+            Аккаунт
+          </Link>
+        </>
+      ) : (
+        <div className="header__auth">
+          <Link className="header__link" to="/signup">
+            Регистрация
+          </Link>
+          <Link className="header__link header__link_type_signin" to="/signin">
+            Войти
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
