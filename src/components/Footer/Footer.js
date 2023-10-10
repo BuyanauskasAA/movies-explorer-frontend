f91bfd9e823acd224484e709409ab12af41a11e7
BuyanauskasAA <Buyanauskas@gmail.com>
2023-10-10 23:19:23 +0600

    feat: сверстать footer для 320px

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="footer">
       <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
       <div className="footer__container">
-        <p className="footer__copyright">&copy; 2023</p>
+        <p className="footer__copyright">&copy;2023</p>
         <ul className="footer__links">
           <li>
             <a className="footer__link" href="https://practicum.yandex.ru/">
