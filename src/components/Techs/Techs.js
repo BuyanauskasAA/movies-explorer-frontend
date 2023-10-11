75c4c2d45c3935e8bb55436b530b7d4498472a43
BuyanauskasAA <Buyanauskas@gmail.com>
2023-10-11 09:56:02 +0600

    fix: добавить ссылки в techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -10,13 +10,41 @@ function Techs() {
         На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.
       </p>
       <ul className="techs__list">
-        <li className="techs__item">HTML</li>
-        <li className="techs__item">CSS</li>
-        <li className="techs__item">JS</li>
-        <li className="techs__item">React</li>
-        <li className="techs__item">Git</li>
-        <li className="techs__item">Express.js</li>
-        <li className="techs__item">mongoDB</li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://html.spec.whatwg.org/">
+            HTML
+          </a>
+        </li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://www.w3.org/Style/CSS/">
+            CSS
+          </a>
+        </li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://www.javascript.com/">
+            JS
+          </a>
+        </li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://react.dev/">
+            React
+          </a>
+        </li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://git-scm.com/">
+            Git
+          </a>
+        </li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://expressjs.com/">
+            Express.js
+          </a>
+        </li>
+        <li className="techs__item">
+          <a className="techs__link" href="https://www.mongodb.com/">
+            mongoDB
+          </a>
+        </li>
       </ul>
     </section>
   );
