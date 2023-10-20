7a74f428ae5e2029f7f77af2fed5d3a9afc9ed69
BuyanauskasAA <Buyanauskas@gmail.com>
2023-10-20 15:39:53 +0600

    fix: не отправлять неизмененные данные пользователя

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,15 +4,35 @@ import CurrentUserContext from '../../contexts/CurrentUserContext';
 
 function Profile({ onLogout, onUpdate, isErrorVisible, errorStatus, isUpdateSucceed }) {
   const user = React.useContext(CurrentUserContext);
+  const [isEditionEnable, setEditionEnable] = React.useState(false);
+  const [formValue, setFormValue] = React.useState({ name: '', email: '' });
+  const [formErrors, setFormErrors] = React.useState({ name: '', email: '' });
+  const [isFormValid, setFormValid] = React.useState(false);
+  const [isFormValueChanged, setFormValuesChanged] = React.useState(false);
 
   React.useEffect(() => {
     setFormValue({ name: user.name, email: user.email });
-  }, []);
+  }, [user]);
 
-  const [isEditionEnable, setEditionEnable] = React.useState(false);
-  const [formValue, setFormValue] = React.useState({});
-  const [formErrors, setFormErrors] = React.useState({ name: '', email: '' });
-  const [isFormValid, setFormValid] = React.useState(false);
+  React.useEffect(() => {
+    handleFormValueChanged();
+  });
+
+  React.useEffect(() => {
+    function handleEscapeClose(event) {
+      if (event.key === 'Escape') {
+        setEditionEnable(false);
+      }
+    }
+
+    if (isEditionEnable) {
+      document.addEventListener('keydown', handleEscapeClose);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleEscapeClose);
+    };
+  }, [isEditionEnable]);
 
   function handleChange(event) {
     const target = event.target;
@@ -33,6 +53,14 @@ function Profile({ onLogout, onUpdate, isErrorVisible, errorStatus, isUpdateSucc
     setEditionEnable(true);
   }
 
+  function handleFormValueChanged() {
+    if (user.name !== formValue.name || user.email !== formValue.email) {
+      setFormValuesChanged(true);
+    } else {
+      setFormValuesChanged(false);
+    }
+  }
+
   return (
     <main className="profile">
       <form className="profile__form">
@@ -45,7 +73,9 @@ function Profile({ onLogout, onUpdate, isErrorVisible, errorStatus, isUpdateSucc
             className="profile__input"
             name="name"
             type="text"
+            pattern="[a-zA-Zа-яА-ЯёЁ\s\-]+"
             required
+            autoComplete="on"
             value={formValue.name}
             disabled={!isEditionEnable}
           />
@@ -59,12 +89,15 @@ function Profile({ onLogout, onUpdate, isErrorVisible, errorStatus, isUpdateSucc
             type="email"
             pattern="[a-zA-Z0-9\.\-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+"
             required
+            autoComplete="on"
             value={formValue.email}
             disabled={!isEditionEnable}
           />
         </label>
         <span className="profile-input-error">{formErrors.email}</span>
-        <span className={`profile-form-update ${isUpdateSucceed ? 'profile-form-update_active' : ''}`}>
+        <span
+          className={`profile-form-update ${isUpdateSucceed ? 'profile-form-update_active' : ''}`}
+        >
           Данные пользователя успешно обновлены
         </span>
         <span className={`profile-form-error ${isErrorVisible ? 'profile-form-error_active' : ''}`}>
@@ -76,9 +109,10 @@ function Profile({ onLogout, onUpdate, isErrorVisible, errorStatus, isUpdateSucc
           <button
             onClick={handleSubmit}
             className={`profile__submit-button ${
-              isFormValid ? '' : 'profile__submit-button_disabled'
+              isFormValid && isFormValueChanged ? '' : 'profile__submit-button_disabled'
             }`}
             type="submit"
+            disabled={!isFormValid || !isFormValueChanged}
           >
             Сохранить
           </button>
